f425bdd9d6f53245f0514aa9ca7b1413166c5374
Dikshya160 <dikshyasharma602@gmail.com>
2025-01-18 10:02:13 +0545

    conversion operation

---
+++ Basics/03_datatypeconversion.js
@@ -0,0 +1,129 @@
+// String to number
+// let score ="abc"
+// console.log(score)
+// console.log(typeof score)
+// let valueInnumber=Number(score)         // [That N in NUmber must be capital here]
+// console.log(valueInnumber)
+// console.log(typeof valueInnumber ) 
+
+//here abc can't convert into number so it give NaN in output
+
+
+
+
+
+// // //number to string
+// let num = 55
+// console.log(num)
+// console.log(typeof num)
+// let stringvalue= String(num)
+// console.log(stringvalue)
+// console.log (typeof stringvalue)
+
+
+
+
+// null to number
+// let score =null
+// console.log(score)
+// console.log(typeof score)
+// let valueInnumber=Number(score)         
+// console.log(valueInnumber)
+// console.log(typeof valueInnumber )
+
+
+
+//undefined to number
+// let score =undefined
+// console.log(score)
+// console.log(typeof score)
+// let valueInnumber=Number(score)         
+// console.log(valueInnumber)
+// console.log(typeof valueInnumber )
+
+
+
+//boolean to number
+
+// let score = true
+// console.log(score)
+// console.log(typeof score)
+// let valueInnumber=Number(score)         
+// console.log(valueInnumber)
+// console.log(typeof valueInnumber )
+
+
+// let score =false
+// console.log(score)
+// console.log(typeof score)
+// let valueInnumber=Number(score)         
+// console.log(valueInnumber)
+// console.log(typeof valueInnumber )
+
+
+
+
+
+     //Note
+// "33" => 33
+// "33abc" => NaN 
+// null => 0
+// Undefined =>Nan
+
+    //In case of boolean
+// true=>1; false=>0
+// "" =>false
+//"Ifthereissmtg" => true
+
+
+
+
+
+
+// //number to string     [S of string must be capital down]
+// let num=55
+// console.log(num)
+// let stringnumber=String(num)
+// console.log(stringnumber)
+// console.log(typeof stringnumber)
+
+
+
+
+
+
+
+            // num/other to boolean
+// 0 to boolean
+    //  let Isloggedin = 0
+    // console.log(Isloggedin)
+    // let booleanIsloggedin =  Boolean(Isloggedin)
+    // console.log(booleanIsloggedin)
+    // console.log(typeof booleanIsloggedin)
+
+// 1 to boolean
+//    let Isloggedin =1
+//    console.log(Isloggedin)
+//    let booleanIsloggedin = Boolean(Isloggedin)
+//    console.log(booleanIsloggedin)
+//    console.log(typeof booleanIsloggedin)  
+
+
+// " " to boolean
+//    let Isloggedin = ""
+//    console.log(Isloggedin)
+//    let booleanIsloggedin = Boolean(Isloggedin)
+//    console.log(booleanIsloggedin)
+//    console.log(typeof booleanIsloggedin)
+   
+
+// "something" to boolean
+//    let Isloggedin ="Diksha"
+//    console.log(Isloggedin)
+//    let booleanIsloggedin = Boolean(Isloggedin)
+//    console.log(booleanIsloggedin)
+//    console.log(typeof booleanIsloggedin)
+   
+
+
+
