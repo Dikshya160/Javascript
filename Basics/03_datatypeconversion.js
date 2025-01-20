04c64f1449b7c2f9c2a2eafc5ee50b9e915a7139
Dikshya160 <dikshyasharma602@gmail.com>
2025-01-20 21:52:39 +0545

    Add new logic

--- Basics/03_datatypeconversion.js
+++ Basics/03_datatypeconversion.js
@@ -186,4 +186,12 @@ let str3 =str1 + str2
 // console.log("2" + 1 )
 
 
+// console.log(1 + "2" + 3);  
+// console.log(1 + +"2" + 3);  
+// console.log(1 + -"1" + "3");
+// console.log("1" - -1 + "2");
 
+// console.log(1 + "2" - "1");  
+// console.log("5" + 3 - 2);  
+// console.log("4" - "2" + "1");
+// console.log(2 + "3" * 2);  
