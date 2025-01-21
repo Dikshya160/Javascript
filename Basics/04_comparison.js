f973fd0d93ebbc191b26f7ab696b82900af4ce6c
Dikshya160 <dikshyasharma602@gmail.com>
2025-01-21 07:00:46 +0545

    comparison

---
+++ Basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log( "2" > 1);
+// console.log( "2">=1)
+// console.log(2<1)
+// console.log( 2==1)
+// console.log(2!=2)
+
+// same output for both string or num
+
+console.log(null>0)
+console.log(null ==0)
+console.log(null >=0)
+
+console.log(undefined > 0);
+console.log(undefined < 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+console.log(undefined <= 0)
+// ans= always false
+
+ 
+                 //===  Compare data types
+console.log("2"=== 2);
+
+
