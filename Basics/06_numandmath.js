32f5fcdc7fcb0f3735dfc344c6351de7490fb03f
Dikshya160 <dikshyasharma602@gmail.com>
2025-01-29 13:49:39 +0545

    num and math

---
+++ Basics/06_numandmath.js
@@ -0,0 +1,58 @@
+// ********************math & num***************
+
+//*****************num****************************
+// const score = 500
+// // console.log(score)
+
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString())     //change num to string 
+// console.log(typeof balance.toString())  //give/check string data type
+// console.log(balance.toString().length)     //give length of string 
+// console.log(balance.toFixed(2));  //give (required) digit after num
+
+
+// const otherNumber= 123.8967357
+// console.log(otherNumber.toPrecision(3)) //give precise value by roundoff
+
+
+// const hundreds= 1000000000000
+// console.log(hundreds.toLocaleString());  //give comma acc to international N.S
+// console.log(hundreds.toLocaleString("en-IN")) //give comma acc to national N.S
+
+
+
+
+
+// *********************math*******************************
+// console.log(Math)
+// console.log(Math.abs(-5))  //change - value to + :absolute value
+// console.log(Math.round(4.51)) //give roundoff value
+// console.log(Math.ceil(6.4)) //give top value: out-7
+// console.log(Math.floor(3.9)) //give below value: out-3
+// console.log(Math.sqrt(25))  //give squareroot : out-5
+// console.log(Math.tan(45))  
+// console.log(Math.min(4,5, 7, 9))  
+// console.log(Math.max(6,7,4,2))
+
+
+console.log(Math.random())  //came random num everytime we do: number came between 0-1
+console.log(Math.random())
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10)+1) //give one num ans
+
+const min= 15
+const max= 20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
+
+
+
+
+
+
