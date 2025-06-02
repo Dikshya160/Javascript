17995160a5d52a2d145629b129e802e422431d51
Dikshya160 <dikshyasharma602@gmail.com>
2025-06-02 23:43:52 +0545

    array

--- Basics-02/01_array.js
+++ Basics-02/01_array.js
@@ -1,15 +1,14 @@
 
 //Array
-/*
-const myArr =[0,1,2,3,4]
-console.log(myArr[0]);
 
-const myHeroines =["dikshya","sushmita"];
+const myArr =[0,1,2,"loveu",false, 4]
+// console.log(myArr[0]);
 
-const myArr2 = new Array(1,2,4,5,)
-console.log(myArr2[3]);
+const myHeroines =["dikshya","sushmita"]
 
-*/
+const myArr2 = new Array(1,2,4,5,8)
+console.log(myArr2[4]);
+
 
 
 
@@ -17,7 +16,7 @@ console.log(myArr2[3]);
 
 //Array method
 
-const myArr =[0,1,2,3,4]
+// const myArr =[0,1,2,3,4]
 // myArr.push("true",8,9)               //Add hunxa
 // myArr.pop()                  //remove last
 // myArr.unshift(9)                //add on first
@@ -39,15 +38,15 @@ const myArr =[0,1,2,3,4]
 
 
 
-//slice,splice:difference
-const mya =[2,3,4,5,6]
-console.log("A", mya); 
+// //slice,splice:difference
+// const mya =[2,3,4,5,6]
+// console.log("A", mya); 
 
-const mya1= mya.slice(1,3)
-console.log("B",mya);
-console.log(mya1);         
+// const mya1= mya.slice(1,3)
+// console.log("B",mya);
+// console.log(mya1);         
 
-const mya2= mya.splice(1,3)
-console.log("C",mya)
-console.log(mya2)
+// const mya2= mya.splice(1,3)
+// console.log("C",mya)
+// console.log(mya2)
 
